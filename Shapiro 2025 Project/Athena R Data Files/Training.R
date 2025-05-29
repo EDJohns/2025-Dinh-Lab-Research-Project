816897e0b30259ad224e1325efede8369e361c0f
EDJohns <81942766+EDJohns@users.noreply.github.com>
2025-05-29 17:43:18 -0500

    EOD Commits

---
+++ Shapiro 2025 Project/Athena R Data Files/Training.R
@@ -0,0 +1,15 @@
+install.packages("pacman")
+
+print("this code has run")
+
+a <- 4
+
+if (a>3){
+  print("a is bigger than 3")
+} else if (a==3) {
+  print("a equals to 3")
+} else {
+  print("a is less than 3")
+}
+
+
