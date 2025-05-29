9673af6df35d090d7d6f389012744da81a9bbc48
EDJohns <81942766+EDJohns@users.noreply.github.com>
2025-05-29 17:36:07 -0500

    Seurat Data Transfer Packages updated
    Seurat data packages updated to transfer in 3 CSV files to be later merged

--- Shapiro 2025 Project/R Code Seurat/Suerat Install and Data Load.R
+++ Shapiro 2025 Project/R Code Seurat/Suerat Install and Data Load.R
@@ -1,20 +1,38 @@
-install.packages("remotes")
-install.packages("Rtools")
-install.packages("SeuratDesk")
 library(Seurat)
-
-load("C:/Users/evanj/Downloads/CosMX_protein_annot_cluster.rda")
-SeuratObject <- get("CosMX_protein_annot_cluster")
-
-loaded_names <- load("C:/Users/evanj/Downloads/CosMX_protein_annot_cluster.rda")
-print(loaded_names)
-
+library(SeuratObject)
 library(SeuratDisk)
 
+# Load the Seurat object from the .rda file
+load("C:/Users/evanj/Downloads/CosMX_protein_annot_cluster.rda")
+
+# Only run this if you're sure the object is named "CosMX_protein_annot_cluster"
+SeuratObject <- get("CosMx_Protein_log_norm")
+
+# Checks to see if the name loaded matches
+# loaded_names <- load("C:/Users/evanj/Downloads/CosMX_protein_annot_cluster.rda")
+# print(loaded_names)
+
+# Save as .h5Seurat
 SaveH5Seurat(SeuratObject, filename = "SeuratObject.h5Seurat")
+
+# Convert to .h5ad (AnnData format for Python use)
 Convert("SeuratObject.h5Seurat", dest = "h5ad", overwrite = TRUE)
 
-# print values
-print("End of Script")
+
+SeuratObject <- DietSeurat(SeuratObject,
+                           counts = TRUE,
+                           data = TRUE,
+                           scale.data = FALSE,
+                           features = NULL,
+                           assays = Assays(SeuratObject),
+                           dimreducs = c("pca", "umap"),
+                           graphs = NULL)
 
 
+seurat_to_adata <- function(out, seu){
+  seu <- Seurat::UpdateSeuratObject(seu)
+  SeuratDisk::SaveH5Seurat(seu, filename = out)
+  SeuratDisk::Convert(out, dest = 'h5ad')
+}
+
+seurat_to_adata("output.h5seurat", SeuratObject)
---
+++ Shapiro 2025 Project/R Code Seurat/SueratDisk Data Transfer.R
@@ -0,0 +1,5 @@
+install.packages("hdf5r")
+install.packages("dplyr")
+
+library(devtools)
+devtools::install_github("mojaveazure/seurat-disk")
